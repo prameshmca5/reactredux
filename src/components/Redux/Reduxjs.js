77d7ca2ee773b46beda93d820166dec601dedab0
Ramesh <rameshkp2@gmail.com>
2020-04-10 17:08:43 +0800

    Crated Redux Reducer Test file.

---
+++ src/components/Redux/Reduxjs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reduxjs =(props) => {
+    return(
+        <div>
+            <h3>Welcome Redux Controller</h3>
+        </div>
+    )
+}
+
+export default Reduxjs;
